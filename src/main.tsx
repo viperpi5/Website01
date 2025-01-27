5b4c37ac76439e34610413fc7a0abd8ad23f25e8
Viper939 <viperpi5@protonmail.com>
2025-01-28 09:13:12 +1100

    Base url

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import Home from './Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/Website01/" element={<Home />}>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Home />
   </StrictMode>,
 )
